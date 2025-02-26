205e0113583f2cdf9ff0308fab16e8d8aa79a465
Tue Do <tuedo2@illinois.edu>
2025-02-25 23:07:50 -0600

    initial commit

---
+++ cnn.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class Net(nn.Module):
+    ''' Models a simple Convolutional Neural Network'''
+    
+    def __init__(self):
+        ''' initialize the network '''
+        super(Net, self).__init__()
+        # 3 input image channel, 6 output channels, 
+        # # 5x5 square convolution kernel
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        # Max pooling over a (2, 2) window
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5) 
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)# 5x5 from image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        ''' the forward propagation algorithm '''
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        
+        return x
